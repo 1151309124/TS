1d8146f0908fe221d0f5038301af89fdef663cb8
1151309124 <1151309124@qq.com>
2022-04-23 00:12:21 +0800

    second

--- type-challenges/hello-world/Hello-world/template.ts
+++ type-challenges/hello-world/Hello-world/template.ts
@@ -1,8 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2022-04-22 18:08:45
- * @LastEditTime: 2022-04-22 18:08:46
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-04-22 21:51:32
+ * @LastEditors: your name
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \新建文件夹e:\vs CODE\TS-practice\type-challenges\hello-world\template.ts
  */
+// expected to be string
+// type HelloWorld = any
+type HelloWorld = string
