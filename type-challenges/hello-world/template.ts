703c31c18090465fbf6d1b7d11325bbb92a880ab
1151309124 <1151309124@qq.com>
2022-04-22 18:11:51 +0800

    first

---
+++ type-challenges/hello-world/template.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-22 18:08:45
+ * @LastEditTime: 2022-04-22 18:08:46
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \新建文件夹e:\vs CODE\TS-practice\type-challenges\hello-world\template.ts
+ */
